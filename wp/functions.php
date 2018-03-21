08ee41061cb1219d3fdd7e425031bd47a281d483
LapTrinhVien1994 <quang.nguyen.7878@gmail.com>
2018-03-22 01:14:49 +0700

    update wp

--- wp/functions.php
+++ wp/functions.php
@@ -19,11 +19,13 @@
 	// add_theme_support('title-tag');
 
 
-	function custom_settings_add_menu(){
-		add_menu_page('Custom settings', 'new settings', 'manage_options', 'custom_settings_page','custom_settings_page', null, 99);
-	}
+	// function custom_settings_add_menu(){
+	// 	add_menu_page('Custom settings', 'new settings', 'manage_options', 'custom_settings_page','custom_settings_page', null, 99);
+	// }
 
-	add_action('admin_menu', 'custom_settings_add_menu');
+	add_action('admin_menu', function(){
+		add_menu_page('Custom settings', 'new settings', 'manage_options', 'slug_name','custom_settings_page', null, 99);
+	});
 
 	function custom_settings_page(){ ?>
 		<div class="wrap">
@@ -31,9 +33,18 @@
 			<form method="post" action="options.php">
 				<?php
 					settings_fields('section');
-					do_settings_sections('theme-options');
+					do_settings_sections('slug_name');
 					submit_button();
 				 ?>
 			</form>
 		</div>
 	<?php }
+
+	function add_custom(){
+		settings_fields('group-name')
+		add_settings_section('id_section', 'Nhập vào title', null, 'general');
+		add_settings_field('id_field', 'Nhập vào giá trị gì đó', null, 'general', 'section', null);
+		register_setting('group-name', )
+	}
+
+	add_action('admin_init', 'add_custom');
