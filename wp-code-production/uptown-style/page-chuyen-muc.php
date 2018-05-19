323d3e9cb5c6cd6cde3dcb4a9fa686de7a483f76
Minh Quang <quang.nguyen@suno.vn>
2018-05-19 19:58:51 +0700

    update wp

--- wp-code-production/uptown-style/page-chuyen-muc.php
+++ wp-code-production/uptown-style/page-chuyen-muc.php
@@ -5,7 +5,7 @@ get_header(); ?>
 <div id="primary" class="content-area">
 
 	<main id="main" class="site-main" role="main">
-
+		<div id="vuongchau-chuyenmuc-content">
 			<?php
 				$taxonomiesList = array(
 				    //'category' => 'Danh mục',
@@ -15,8 +15,8 @@ get_header(); ?>
 				$taxonomies = get_taxonomies();
 				foreach ( $taxonomies as $taxonomy ) {
 				    if(array_key_exists($taxonomy, $taxonomiesList)){
-				        echo '<h3>' . $taxonomiesList[$taxonomy] . '</h3>';
-				        echo '<ul>';
+				        echo '<h3 class="vuongchau-chuyenmuc-title">' . $taxonomiesList[$taxonomy] . '</h3>';
+				        echo '<ul class="vuongchau-chuyenmuc-ul">';
 				        $terms = get_terms(array(
 				            'taxonomy' => $taxonomy,
 				            'hide_empty' => false,
@@ -25,14 +25,15 @@ get_header(); ?>
 				        foreach ($terms as $term) { #print_r($term);
 				        	$link = get_site_url().'/'.$term->taxonomy.'/'.$term->slug;
 				        	$posts_count = (string)$term->count;
-							$text = 'Chuyên mục về '.$term->name;
-				            echo '<li><a href="'.$link.'">'.$text.' ( '.$posts_count.' bài viết )'.'</a></li>';
+							$text = wp_is_mobile() ? 'Về '.$term->name : 'Chuyên mục về '.$term->name;
+							$post_count_text = wp_is_mobile() ? ' bài' : ' bài viết';
+				            echo '<li class="vuongchau-chuyenmuc-item"><a href="'.$link.'">'.$text.' <span class="vuongchau-chuyenmuc-item-postcount"> ( '.$posts_count.$post_count_text.' ) </span> '.'</a></li>';
 				        }
 				        echo '</ul>'; 
 				    }   
 				}
 			?>
-
+		</div> <!-- #vuongchau-chuyenmuc-content-->
 	</main><!-- #main -->
 
 </div><!-- #primary -->
